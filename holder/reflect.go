d409ac5a52fb6726e26bfaa7cea1b9411eb381d0
js103 <643929860@qq.com>
2019-01-21 14:21:51 +0700

    reflect

---
+++ holder/reflect.go
@@ -0,0 +1,29 @@
+package main
+
+//反射
+import (
+	"fmt"
+	"reflect"
+)
+
+type Bird struct {
+	Name           string
+	LifeExpectance int
+}
+
+func (b *Bird) Fly() {
+	fmt.Println("I am flying...")
+}
+
+func main() {
+	sparrow := &Bird{"Sparrow", 3}
+	s := reflect.ValueOf(sparrow).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
+	}
+}
+
+// 0: Name string = Sparrow
+// 1: LifeExpectance int = 3
